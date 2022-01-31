29f7b9801293bfb143276ee027e1b630b0f953ba
alimirmohamma <97875042+alimirmohamma@users.noreply.github.com>
2022-01-31 21:28:12 +0330

    Add files via upload

---
+++ win activation/setup2.py
@@ -0,0 +1,2 @@
+import os
+os.startfile("kms atuo ls.exe")
---
+++ win activation/show.py
@@ -0,0 +1,4 @@
+import time
+import os
+print("welcome! please open the txt file to read how to install")
+os.startfile("read me(how to install).txt")
